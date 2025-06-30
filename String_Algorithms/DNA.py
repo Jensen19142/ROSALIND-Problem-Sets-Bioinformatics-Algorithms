b0a9bb1eb061ad0473f7fbe374679f0da92690e6
Jensen Tan <tzhjensen142@gmail.com>
2025-06-30 12:02:02 +0800

    modified:   String_Algorithms/DNA.py 	modified:   String_Algorithms/REVC.py 	modified:   String_Algorithms/RNA.py

--- String_Algorithms/DNA.py
+++ String_Algorithms/DNA.py
@@ -7,20 +7,4 @@ Return: Four integers (separated by spaces) counting the respective number of ti
 '''
 
 dataset = input("Type in DNA String: ")
-
-adenine_count = 0
-cytosine_count = 0
-guanine_count = 0
-thymine_count = 0
-
-for char in range(len(dataset)):
-    if dataset[char] == "A":
-        adenine_count += 1
-    if dataset[char] == "C":
-        cytosine_count += 1
-    if dataset[char] == "G":
-        guanine_count += 1
-    if dataset[char] == "T":
-        thymine_count += 1
-
-print(adenine_count, cytosine_count, guanine_count, thymine_count)
+print(dataset.count("A"),dataset.count("C"),dataset.count("G"),dataset.count("T"))
--- String_Algorithms/RNA.py
+++ String_Algorithms/RNA.py
@@ -3,13 +3,6 @@
 """
 Given: A DNA string t having length at most 1000 nt.
 Return: The transcribed RNA string of t.
-
 """
 
-dataset = input("Type in DNA String: ")
-
-for char in range(len(dataset)):
-    if dataset[char] == "T":
-        dataset = dataset.replace('T', 'U')
-
-print(dataset)
+print(input("Type in DNA String: ").replace('T', 'U'))
